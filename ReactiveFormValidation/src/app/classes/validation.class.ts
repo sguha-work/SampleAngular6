a456dae16e3ed399dfd29b5ffc9d8f91aea36c4e
sguha-work <sguha1988.life@gmail.com>
2018-09-28 15:46:47 +0530

    folders deleted

--- ReactiveFormValidation/src/app/classes/validation.class.ts
+++
@@ -1,8 +0,0 @@
-import {AbstractControl} from '@angular/forms';
-
-export function validateOfficeEmail(controls: AbstractControl): any {
-    if(!controls.value.includes('ericsson.com')) {
-        return {"validOfficeEmail": true};
-    }
-    return null;
-}
